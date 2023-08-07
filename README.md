11ab80396b962d0555d90bc10ab8f6a6a0f6a94f
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-08-07 21:21:19 +0200

    progress

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is problem 57 from projecteuler.net 
+
+To initiate build: 
+
+1/ CC=clang CXX=clang++ CXX_LD=clang meson setup builddir
+2/ ninja -C builddir 
+
+
--- main.cpp
+++ main.cpp
@@ -4,16 +4,34 @@
 
 class RationalNumber {
     public: 
-        RationalNumber( int numerator, int denominator ) { m_numerator = numerator; m_denominator = denominator; }; 
+        RationalNumber( unsigned int numerator, unsigned int denominator ) { m_numerator = numerator; m_denominator = denominator; }; 
         
         friend RationalNumber one_over_two_plus( const RationalNumber & other );
         friend RationalNumber plus_one( const RationalNumber & other );
 
         friend std::ostream& operator<<(std::ostream& os, const RationalNumber& n);
+        bool numerator_has_more_digits_than_denominator();
     private: 
-        int m_denominator; 
-        int m_numerator;
+        unsigned int m_denominator; 
+        unsigned int m_numerator;
 };
+
+unsigned int number_of_digits( unsigned int & n){ 
+    int k = 1; 
+    int trial = n;
+    while ( (trial = trial/10) ) { 
+        k++;
+    }
+    return k; 
+}
+
+bool RationalNumber::numerator_has_more_digits_than_denominator(){ 
+
+    if ( number_of_digits( this->m_numerator )>number_of_digits( this->m_denominator ) )  { 
+        return true;
+    }
+    return false;
+}
 
 RationalNumber one_over_two_plus( const RationalNumber & other ) { 
     return RationalNumber( other.m_denominator, 2*other.m_denominator+other.m_numerator ); 
@@ -34,9 +52,17 @@ int main()
 
     RationalNumber iter(1,2); 
 
-    for ( int i = 0; i<10; i++ ) { 
+    int answer{0};
+
+    for ( int i = 0; i<(1000-1); i++ ) { 
         iter =  one_over_two_plus( iter );
         RationalNumber trial = plus_one( iter );
         std::cout<< trial;
+        if ( trial.numerator_has_more_digits_than_denominator() ) { 
+            std::cout<< "here!" << std::endl;
+            answer++;
+        }
     }
+    std::cout<< "answer" << answer << std::endl; 
+
 }
