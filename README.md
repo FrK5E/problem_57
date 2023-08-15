e824b1a05e402d4f9d85779752a4ffc5b33629e6
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-08-15 21:37:14 +0200

    Needed to use library for arbitrary length integers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This is problem 57 from projecteuler.net
 
 To initiate build: 
 
-1/ CC=clang CXX=clang++ CXX_LD=clang meson setup builddir
-2/ ninja -C builddir 
+1/ cmake -B build -G Ninja 
+2/ ninja -C build 
 
 
--- main.cpp
+++ main.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
+#include <sstream>
 
 
+#include <NTL/ZZ.h>
 
-class RationalNumber {
+using namespace NTL;
+
+template <typename T> class RationalNumber {
     public: 
-        RationalNumber( unsigned int numerator, unsigned int denominator ) { m_numerator = numerator; m_denominator = denominator; }; 
+        RationalNumber( T numerator, T denominator ) { m_numerator = numerator; m_denominator = denominator; }; 
         
-        friend RationalNumber one_over_two_plus( const RationalNumber & other );
-        friend RationalNumber plus_one( const RationalNumber & other );
-
-        friend std::ostream& operator<<(std::ostream& os, const RationalNumber& n);
+        template <typename S> 
+        friend RationalNumber<S> one_over_two_plus( const RationalNumber<S> & other );
+        template <typename S> 
+        friend RationalNumber<S> plus_one( const RationalNumber<S> & other );
+        template <typename S>
+        friend std::ostream& operator<<(std::ostream& os, const RationalNumber<S>& n);
         bool numerator_has_more_digits_than_denominator();
     private: 
-        unsigned int m_denominator; 
-        unsigned int m_numerator;
+        T m_denominator; 
+        T m_numerator;
 };
 
-unsigned int number_of_digits( unsigned int & n){ 
-    int k = 1; 
-    int trial = n;
-    while ( (trial = trial/10) ) { 
-        k++;
+namespace BoilerPlaceCode {     
+    std::string conver_to_string( const ZZ & number ) { 
+        std::ostringstream os;
+        os << number; 
+        std::string s = os.str();
+        return s; 
     }
-    return k; 
 }
 
-bool RationalNumber::numerator_has_more_digits_than_denominator(){ 
+
+template <typename T> size_t number_of_digits( const T & n){ 
+    std::string s = std::to_string(n); 
+    return s.length(); 
+};
+
+template<>        // specialization for T = ZZ
+size_t number_of_digits( const ZZ & n ){ 
+    std::string s = BoilerPlaceCode::conver_to_string(n); 
+    return s.length();
+}
+
+template <typename T> 
+bool RationalNumber<T>::numerator_has_more_digits_than_denominator(){ 
 
     if ( number_of_digits( this->m_numerator )>number_of_digits( this->m_denominator ) )  { 
         return true;
@@ -33,15 +52,18 @@ bool RationalNumber::numerator_has_more_digits_than_denominator(){
     return false;
 }
 
-RationalNumber one_over_two_plus( const RationalNumber & other ) { 
-    return RationalNumber( other.m_denominator, 2*other.m_denominator+other.m_numerator ); 
+template <typename T> 
+RationalNumber<T> one_over_two_plus( const RationalNumber<T> & other ) { 
+    return RationalNumber<T>( other.m_denominator, 2*other.m_denominator+other.m_numerator ); 
 }
 
-RationalNumber plus_one( const RationalNumber & other ) { 
-    return RationalNumber( other.m_numerator+other.m_denominator, other.m_denominator );
+template <typename T> 
+RationalNumber<T> plus_one( const RationalNumber<T> & other ) { 
+    return RationalNumber<T>( other.m_numerator+other.m_denominator, other.m_denominator );
 }
 
-std::ostream& operator<<(std::ostream& os, const RationalNumber& n){ 
+template <typename T> 
+std::ostream& operator<<(std::ostream& os, const RationalNumber<T>& n){ 
     os << n.m_numerator << '/' << n.m_denominator << std::endl;
     return os;
 }
@@ -50,14 +72,14 @@ std::ostream& operator<<(std::ostream& os, const RationalNumber& n){
 int main()
 {
 
-    RationalNumber iter(1,2); 
+    RationalNumber<ZZ> iter(to_ZZ(1),to_ZZ(2)); 
 
     int answer{0};
 
-    for ( int i = 0; i<(1000-1); i++ ) { 
+    for ( int i = 2; i<=(1000); i++ ) { 
         iter =  one_over_two_plus( iter );
-        RationalNumber trial = plus_one( iter );
-        std::cout<< trial;
+        auto trial = plus_one( iter );
+        std::cout << i << " " << trial;
         if ( trial.numerator_has_more_digits_than_denominator() ) { 
             std::cout<< "here!" << std::endl;
             answer++;
